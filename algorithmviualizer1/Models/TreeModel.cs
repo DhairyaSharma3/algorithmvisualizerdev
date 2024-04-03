694a7758a70f005ade546c405d3c544742231f7e
wwwte <wwwte@DHAIRYA>
2024-04-04 01:04:31 +0530

    Add project files.

---
+++ algorithmviualizer1/Models/TreeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace algorithmviualizer1.Models
+{
+    public class TreeModel
+    {
+        public int Value { get; set; }
+        public TreeModel Left { get; set; }
+        public TreeModel Right { get; set; }
+    }
+}
